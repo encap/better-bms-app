6eb082cb05a1e869bff31c424f3cf68d373e2219
encap <encapsulation4@gmail.com>
2022-07-13 13:41:09 +0200

    feat: tweak bar gauge animation duration

--- src/components/molecules/BarGauge/index.tsx
+++ src/components/molecules/BarGauge/index.tsx
@@ -16,19 +16,19 @@ const BarGauge = ({ value, max, duration, ...gaugeValueProps }: BarGaugeProps) =
     if (gaugeRef.current && rippleContainerRef.current) {
       const percent = (Math.min(value || 0, max) / max) * 100;
 
-      const calculatedDuration = Math.max(duration * 1.25, 100);
+      const calculatedDuration = Math.max(duration * 1.1, 100);
       gaugeRef.current.style.transitionDuration = `${calculatedDuration}ms`;
       gaugeRef.current.style.transform = `translate3d(0, -${percent}%, 0)`;
 
       if (
         gaugeValueProps.background !== 'white' &&
         percent > 30 &&
-        percent - previousRef.current > 20
+        percent - previousRef.current > 10
       ) {
         rippleContainerRef.current.style.animation = 'none';
         rippleContainerRef.current.offsetHeight;
         rippleContainerRef.current.style.animation = `ripple ease-out ${
-          calculatedDuration * 1.25
+          calculatedDuration * 1.4
         }ms`;
       }
 
