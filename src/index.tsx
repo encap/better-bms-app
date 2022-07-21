38bd6d0620ac6375f1869e76a45d9dfb175ca30f
encap <encapsulation4@gmail.com>
2022-07-21 18:29:31 +0200

    feat: cache first service worker

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import DeviceProvider from 'components/providers/DeviceProvider';
 import App from 'components/organisms/App';
 import { CssBaseline, GeistProvider, Themes } from '@geist-ui/core';
 import { ThemeProvider } from 'styled-components';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const customDarkTheme = Themes.createFromDark({
   type: 'customDark',
@@ -31,6 +32,11 @@ root.render(
   </ThemeProvider>
   // </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
